dd270e32cef30de1835ce8a29b1f6aa7a6dacf19
Andrew Snodgrass <pointlander@gmail.com>
2024-06-12 10:06:41 -0500

    WIP entropy model

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+
+	"github.com/pointlander/gradient/tf64"
 )
 
 type Example struct {
@@ -44,4 +46,38 @@ func main() {
 	}
 	fmt.Println("test", test)
 	fmt.Println("train", train)
+
+	x, y := 0, 0
+	for _, value := range sets[0].Train {
+		if len(value.Input) > y {
+			y = len(value.Input)
+		}
+		if len(value.Input[0]) > x {
+			x = len(value.Input[0])
+		}
+		if len(value.Output) > y {
+			y = len(value.Output)
+		}
+		if len(value.Output[0]) > x {
+			x = len(value.Output[0])
+		}
+	}
+	size := x * y * 10
+	input := tf64.NewV(size, len(sets[0].Train))
+	input.X = input.X[:cap(input.X)]
+
+	set := tf64.NewSet()
+	set.Add("q", size, size)
+	set.Add("k", size, size)
+	set.Add("v", size, size)
+
+	q := tf64.Mul(set.Get("q"), input.Meta())
+	k := tf64.Mul(set.Get("k"), input.Meta())
+	v := tf64.Mul(set.Get("v"), input.Meta())
+	attention := tf64.T(tf64.Mul(tf64.Softmax(tf64.Mul(q, k)), tf64.T(v)))
+	output := tf64.Entropy(tf64.Softmax(attention))
+	output(func(a *tf64.V) bool {
+		fmt.Println(a.S)
+		return true
+	})
 }
